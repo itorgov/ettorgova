15e65947f621361a6389e72ea990af86467dd4dd
Иван Торгов <likecod@gmail.com>
2016-03-27 21:21:08 +0300

    Meta тэги для iOS Safari.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,11 @@
     @section('meta')
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        {{--Android Chrome--}}
         <meta name="theme-color" content="#63C6AE">
+        {{--iOS Safari--}}
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     @show
     @section('css')
     @show
